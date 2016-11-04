c78ce9b15f2a5b675acc30e37f0e268164674927
amos <amos@SS-LiYingchun.local>
2016-11-04 16:57:05 +0800

    Initial Commit

---
+++ AmosPasswordStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AmosPasswordStore
+//
+//  Created by amos on 2016/11/4.
+//  Copyright © 2016年 amos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
